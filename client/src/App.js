1ecc723db855a820c9ff3c70079c775da0087f8b
Santi <100055564+Santi-Rijal@users.noreply.github.com>
2023-08-21 17:53:08 -0300

    Navbar done

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import Layout from './components/Layout';
 import Error from './components/Error';
 import Home from './pages/Home'
 import './style/App.scss';
+import'./style/fonts.scss';
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
@@ -21,6 +22,11 @@ const router = createBrowserRouter([
 function App() {
   return (
     <div className="app">
+      <div className="logo-container">
+        <span className="blur" id="top-blur"/>
+        <p className="logo">NovaMall</p>
+        <span className="blur" id="bottom-blur"/>
+      </div>
       <RouterProvider router={router} />
     </div>
   );
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { FaOpencart } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+
+  const [clickedId, setClickedId] = useState("Home");
+
+  const onNavItemClick = (navItemId) => {
+    setClickedId(navItemId);
+  }
+
   return (
     <div className="navbar-container">
-      <nav className="navbar">
-        
-      </nav>
+      <div className="navbar">
+        <Link className="link">
+          <span className={clickedId === "Home" ? "clicked" : ""} onClick={() => onNavItemClick("Home")}>Home</span>
+        </Link>
+
+        <Link className="link">
+          <span className={clickedId === "Men's Clothing" ? "clicked" : ""} onClick={() => onNavItemClick("Men's Clothing")}>Men's Clothing</span>
+        </Link>
+
+        <Link className="link">
+          <span className={clickedId === "Women's Clothing" ? "clicked" : ""} onClick={() => onNavItemClick("Women's Clothing")}>Women's Clothing</span>
+        </Link>
+
+        <Link className="link">
+          <span className={clickedId === "Jewlery" ? "clicked" : ""} onClick={() => onNavItemClick("Jewlery")}>Jewelery</span>
+        </Link>
+
+        <Link className="link">
+          <span className={clickedId === "Cart" ? "clicked" : ""} onClick={() => onNavItemClick("Cart")}><FaOpencart /></span>
+        </Link>
+
+        <Link className="link">
+          <span className={clickedId === "Login" ? "clicked" : ""} id="login" onClick={() => onNavItemClick("Login")}>Login</span>
+        </Link>
+      </div>
     </div>
   )
 }
