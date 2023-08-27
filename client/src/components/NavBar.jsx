5672aa96d5910678e8bbe42bfb2b2dfe0ef56aca
Santi <100055564+Santi-Rijal@users.noreply.github.com>
2023-08-27 15:17:04 -0300

    Added a bubble above the cart to display how many items are in there and fixed nav item style issues.

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -9,7 +9,8 @@ const NavBar = () => {
 
   const [clickedId, setClickedId] = useState("Home");
   const [showNavBar, setShowNavBar] = useState(true);
-  const [currWindowSize, setCurrWindowSize] = useState(window.innerWidth)
+  const [currWindowSize, setCurrWindowSize] = useState(window.innerWidth);
+  const cart = JSON.parse(window.localStorage.getItem("cart-items")) || [];
 
   // Set the id of what navbar item has been clicked.
   const onNavItemClick = (navItemId) => {
@@ -54,7 +55,10 @@ const NavBar = () => {
             <AiOutlineMenuUnfold onClick={onUnFoldNav}/>
             <span>{clickedId}</span>
             <Link className="link">
-              <FaOpencart className={clickedId === "Cart" ? "clicked" : ""} onClick={() => onNavItemClick("Cart")}/>
+              <div className="cart-container">
+                {cart.length > 0 && <span className="cart-items">{cart.length}</span>}
+                <span className={clickedId === "Cart" ? "clicked" : ""} onClick={() => onNavItemClick("Cart")}><FaOpencart /></span>
+              </div>            
             </Link>
           </div>
         ) : (
@@ -83,28 +87,31 @@ const NavBar = () => {
             </div>
           ) : (
             <div className="navbar">
-              <Link className="link" to={"/"}>
-                <span className={clickedId === "Home" ? "clicked" : ""} onClick={() => onNavItemClick("Home")}>Home</span>
+              <Link className={`link ${clickedId === "Home" ? "clicked" : ""}`} onClick={() => onNavItemClick("Home")} to={"/"}>
+                <span >Home</span>
               </Link>
 
-              <Link className="link" to={"/men's-clothing"}>
-                <span className={clickedId === "Men's Clothing" ? "clicked" : ""} onClick={() => onNavItemClick("Men's Clothing")}>Men's Clothing</span>
+              <Link className={`link ${clickedId === "Men's Clothing" ? "clicked" : ""}`} onClick={() => onNavItemClick("Men's Clothing")} to={"/men's-clothing"}>
+                <span>Men's Clothing</span>
               </Link>
 
-              <Link className="link" to={"/women's-clothing"}>
-                <span className={clickedId === "Women's Clothing" ? "clicked" : ""} onClick={() => onNavItemClick("Women's Clothing")}>Women's Clothing</span>
+              <Link className={`link ${clickedId === "Women's Clothing" ? "clicked" : ""}`} onClick={() => onNavItemClick("Women's Clothing")} to={"/women's-clothing"}>
+                <span>Women's Clothing</span>
               </Link>
 
-              <Link className="link" to={"/jewelry"}>
-                <span className={clickedId === "Jewelry" ? "clicked" : ""} onClick={() => onNavItemClick("Jewelry")}>Jewelry</span>
+              <Link className={`link ${clickedId === "Jewelry" ? "clicked" : ""}`} onClick={() => onNavItemClick("Jewelry")} to={"/jewelry"}>
+                <span>Jewelry</span>
               </Link>
 
-              <Link className="link" to={"/cart"}>
-                <span className={clickedId === "Cart" ? "clicked" : ""} onClick={() => onNavItemClick("Cart")}><FaOpencart /></span>
+              <Link className={`link ${clickedId === "Cart" ? "clicked" : ""}`} onClick={() => onNavItemClick("Cart")} to={"/cart"}>
+                <div className="cart-container">
+                  {cart.length > 0 && <span className="cart-items">{cart.length}</span>}
+                  <span><FaOpencart /></span>
+                </div>
               </Link>
 
-              <Link className="link" to={"/login"}>
-                <span className={clickedId === "Login" ? "clicked" : ""} id="login" onClick={() => onNavItemClick("Login")}>Login</span>
+              <Link className={`link ${clickedId === "Login" ? "clicked" : ""}`} id="login" onClick={() => onNavItemClick("Login")} to={"/login"}>
+                <span>Login</span>
               </Link>
             </div>
           )
