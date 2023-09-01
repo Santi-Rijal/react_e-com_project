53a5eff0762f80918cfd56200e346c0354e159cc
Santi <100055564+Santi-Rijal@users.noreply.github.com>
2023-09-01 18:27:25 -0300

    Pages commented and cleanup

--- client/src/pages/Jewelry.jsx
+++ client/src/pages/Jewelry.jsx
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import ItemCard from '../components/ItemCard';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-import { Link } from 'react-router-dom';
+// Components.
+import ItemCard from "../components/ItemCard";
 
+// A page containing all the jewelry.
 const Jewelery = () => {
   const [jewelry, setJewelry] = useState([]);
   const [pagenum, setPagenum] = useState(1);
@@ -12,53 +15,89 @@ const Jewelery = () => {
     const fetchData = async () => {
       const url = `https://apidojo-forever21-v1.p.rapidapi.com/products/search?query=jewelry&rows=12&start=${start}`;
       const options = {
-        method: 'GET',
+        method: "GET",
         headers: {
-          'X-RapidAPI-Key': '5c8c14735bmsh3ddd43190f166dfp1d6a64jsn960d37cf18b5',
-          'X-RapidAPI-Host': 'apidojo-forever21-v1.p.rapidapi.com'
-        }
+          "X-RapidAPI-Key": process.env.REACT_APP_FOREVER21_API_KEY,
+          "X-RapidAPI-Host": "apidojo-forever21-v1.p.rapidapi.com",
+        },
       };
 
       try {
-        const res = await fetch(url, options);
-        const data = await res.json();
-        setJewelry(data.response.docs);
+        const res = await axios.request(url, options);
+        const data = await res.data.response.docs;
+        setJewelry(data);
       } catch (error) {
         console.error(error);
       }
-    }
+    };
 
     fetchData();
-
   }, [pagenum, start]);
 
+  // A function that handles page changes.
   const handlePageChange = (e) => {
-    const clickedPage = +e.target.getAttribute("value");
-    const start = clickedPage === 1 ? (12 * clickedPage) - 12 : (12 * clickedPage) - 11;
+    const clickedPage = +e.target.getAttribute("value"); // Get which page we are on.
+    const start =
+      clickedPage === 1 ? 12 * clickedPage - 12 : 12 * clickedPage - 11; // Starting row for the api.
 
     setStart(start);
     setPagenum(clickedPage);
-  }
-  
+  };
+
   return (
     <div className="clothing">
       <div className="items">
-        {jewelry.map(itemObj => (
-          <Link className="link" to={itemObj.pid} state={{ itemObj: itemObj }} key={itemObj.pid}>
+        {jewelry.map((itemObj) => (
+          <Link
+            className="link"
+            to={itemObj.pid}
+            state={{ itemObj: itemObj }}
+            key={itemObj.pid}
+          >
             <ItemCard key={itemObj.pid} itemObj={itemObj} />
           </Link>
         ))}
       </div>
 
       <div className="pages">
-        <span value="1" onClick={handlePageChange} className={pagenum === 1 ? "clicked" : ""}>1</span>
-        <span value="2" onClick={handlePageChange} className={pagenum === 2 ? "clicked" : ""}>2</span>
-        <span value="3" onClick={handlePageChange} className={pagenum === 3 ? "clicked" : ""}>3</span>
-        <span value="4" onClick={handlePageChange} className={pagenum === 4 ? "clicked" : ""}>4</span>
-        <span value="5" onClick={handlePageChange} className={pagenum === 5 ? "clicked" : ""}>5</span>
+        <span
+          value="1"
+          onClick={handlePageChange}
+          className={pagenum === 1 ? "clicked" : ""}
+        >
+          1
+        </span>
+        <span
+          value="2"
+          onClick={handlePageChange}
+          className={pagenum === 2 ? "clicked" : ""}
+        >
+          2
+        </span>
+        <span
+          value="3"
+          onClick={handlePageChange}
+          className={pagenum === 3 ? "clicked" : ""}
+        >
+          3
+        </span>
+        <span
+          value="4"
+          onClick={handlePageChange}
+          className={pagenum === 4 ? "clicked" : ""}
+        >
+          4
+        </span>
+        <span
+          value="5"
+          onClick={handlePageChange}
+          className={pagenum === 5 ? "clicked" : ""}
+        >
+          5
+        </span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Jewelery
+export default Jewelery;
